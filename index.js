b4af0d8180aac64f50dddcba77aeb1a14a5dbe27
FransJoakim <fransjlt@gmail.com>
2022-01-25 21:09:34 +0100

    starting

---
+++ index.js
@@ -0,0 +1,27 @@
+class Subject {
+    constructor(name, period) {
+        this.name = name;
+        this.period = period;
+    }
+}
+
+class Lesson {
+}
+
+class Class {
+}
+
+class Group {
+}
+
+class Teacher {
+}
+
+class Student {
+}
+
+class Course {
+}
+
+class Activity {
+}
---
+++ scheduler-client.js
@@ -0,0 +1,6 @@
+const timeElements = [12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12];
+
+function addHour() {
+    const hour = document.createElement('div');
+    hour.setAttribute('class', 'hour');
+} 
